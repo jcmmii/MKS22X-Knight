610960022c6dde6caf36e983c0fa1c76d56aa51e
Justin Chen <jchen06@stuy.edu>
2019-02-24 18:07:33 -0500

    Created testing file

---
+++ Driver.java
@@ -0,0 +1,7 @@
+public class Driver {
+  public static void main(String[] args) {
+    KnightBoard test = new KnightBoard(8,8);
+    System.out.println(test.solve(0,0));
+    System.out.println(test);
+  }
+}
