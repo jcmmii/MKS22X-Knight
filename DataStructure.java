1479a0382eb95115a5e5d2ca209a85dc33377ea5
Justin Chen <jchen06@stuy.edu>
2019-02-26 19:28:49 -0500

    Data structure class created to sort/store coords, amt of outgoing moves

---
+++ DataStructure.java
@@ -0,0 +1,12 @@
+//Stores & Sorts all coordinates and stores the amount of outgoing moves
+
+public class DataStructure {
+  private int row, column, moves;
+
+  public DataStructure (int ROW, int COLUMN, int MOVES) {
+    row = ROW;
+    column = COLUMN;
+    moves = MOVES;
+  }
+
+}
